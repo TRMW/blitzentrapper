eb539dd5015ca4dc1422b0684db531da37e496ee
Matt @ IODA <wrattmight@gmail.com>
2010-05-26 16:26:10 -0700

    using config instead of require for all gems and adding correct timezone

--- config/environment.rb
+++ config/environment.rb
@@ -5,13 +5,8 @@
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
-require 'open-uri'
-require 'hpricot'
-# require 'rack-rewrite'
 
 Rails::Initializer.run do |config|
-
-# config.gem 'rack-rewrite'
 
 # config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
 #   # redirect any other domain to www.blitzentrapper.net
@@ -36,14 +31,16 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'authlogic'
+  congif.gem 'hpricot'
   config.gem 'json'
   config.gem 'httparty'
   config.gem 'feedzirra'
+  # config.gem 'rack-rewrite'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  config.plugins = [ :nested_form ]
+  # config.plugins = [ :nested_form ]
 
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
@@ -54,7 +51,7 @@ Rails::Initializer.run do |config|
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = 'UTC'
+  config.time_zone = 'UTC -08:00'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
