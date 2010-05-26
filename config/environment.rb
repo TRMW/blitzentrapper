e871f504a575f7329ecf925cc9ed40bb992688bc
Matt @ IODA <wrattmight@gmail.com>
2010-05-26 16:33:43 -0700

    re-adding rack-rewrite

--- config/environment.rb
+++ config/environment.rb
@@ -35,7 +35,7 @@ Rails::Initializer.run do |config|
 	config.gem 'json'
 	config.gem 'httparty'
 	config.gem 'feedzirra'
-	# config.gem 'rack-rewrite'
+	config.gem 'rack-rewrite'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
