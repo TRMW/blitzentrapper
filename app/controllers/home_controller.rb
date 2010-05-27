afc3d19065f6e6d3442b07bf72d6bd25b01febc6
Matt @ IODA <wrattmight@gmail.com>
2010-05-26 17:51:44 -0700

    direct all old files to static holding page for now

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
 	
   #redirect to index
   def redirect
-  	render 'public/index.html', :status => :moved_temporarily # remove once site is unveiled
+  	render 'public/index.html', :layout => false, :status => :moved_temporarily # remove once site is unveiled
     # redirect_to :action => 'index', :status => :moved_permanently
   end
 end
