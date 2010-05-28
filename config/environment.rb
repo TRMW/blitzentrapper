6ff21dd1cbde2fa4230d38fc7b1246a86f59eaca
Matt Wright <wrattmight@gmail.com>
2010-05-28 10:53:21 -0700

    updated some thing

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,6 @@ Rails::Initializer.run do |config|
 	config.gem 'httparty'
 	config.gem 'feedzirra'
 	config.gem 'will_paginate'
-	config.gem 'spreadsheet'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
