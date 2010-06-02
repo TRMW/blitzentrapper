a6f8bcadd2c1d0630448c623512cc611129c3fe7
Matt @ IODA <wrattmight@gmail.com>
2010-06-02 11:12:00 -0700

    trying to fix topic sort order

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,7 +3,7 @@ class TopicsController < ApplicationController
 	before_filter :store_location, :only => [ :index, :show ]
 	
   def index
-    @topics = Topic.paginate :page => params[:page], :order => 'updated_at DESC'
+    @topics = Topic.paginate :page => params[:page], :order => "posts.updated_at"
     @topic = Topic.new
     @topic.posts.build
   end
