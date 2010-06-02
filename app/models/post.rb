71d469be72ec66f0f82dc66c6f5ca2c0298235cf
Matt @ IODA <wrattmight@gmail.com>
2010-06-02 16:53:22 -0700

    update topic freshness on post delete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,17 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
   belongs_to :postable, :polymorphic => true
-  after_create :update_topic
+  after_create :update_topic_freshness
+  after_destroy :reset_topic_freshness
   
-  def update_topic
+  def update_topic_freshness
   	postable.last_post_date = created_at
+  	postable.save
+  end
+  
+  def reset_topic_freshness
+  	most_recent_post = postable.posts(:order => :created_at).last
+  	postable.last_post_date = most_recent_post.created_at
   	postable.save
   end
 end
