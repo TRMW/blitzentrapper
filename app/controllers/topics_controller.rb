5301a1cf4b6664aa0282213b501502b2cfdb64be
Matt Wright <wrattmight@gmail.com>
2010-06-06 17:45:17 -0700

    adding ILIKE to searches

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -58,7 +58,7 @@ class TopicsController < ApplicationController
   	@query = params[:query].strip if params[:query]
   	
   	if @query and request.xhr?
-      @topics = Topic.find(:all, :conditions => ["title ILIKE ?", "%#{@query}%"], :order => "date DESC")     
+      @topics = Topic.find(:all, :conditions => ["title ILIKE ?", "%#{@query}%"], :order => "last_post_date DESC")     
       render :partial => "search", :layout => false
     end
 	end
