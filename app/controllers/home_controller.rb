eb9f2d9537968de0e652a9ed952c4a8fa4bbfdf1
Matt Wright <wrattmight@gmail.com>
2010-06-08 02:00:33 -0700

    changing front page to blog for release date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ class HomeController < ApplicationController
 		response = HTTParty.get('http://blitzentrapper.tumblr.com/api/read', :query => {:num => '3'})
 		@blogposts = response['tumblr']['posts']
 		@latest_release = Record.find(:last, :order => "release_date ASC")
-		@topics = Topic.find(:all, :order => "last_post_date DESC", :limit => 3)
-		@postedshows = Show.find(:all, :order => "last_post_date DESC", :limit => 3)
+		@topics = Topic.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
+		@postedshows = Show.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
 		@shows = Show.today_forward(3)
 	end
 	
