381196f6fdae32d608d52d2623db298858ab2db7
Matt Wright <wrattmight@gmail.com>
2010-06-12 11:47:54 -0700

    show more posts on front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
-		response = HTTParty.get('http://blitzentrapper.tumblr.com/api/read', :query => {:num => '3'})
+		response = HTTParty.get('http://blitzentrapper.tumblr.com/api/read', :query => {:num => '10'})
 		@blogposts = response['tumblr']['posts']
 		@latest_release = Record.find(:last, :order => "release_date ASC")
 		@topics = Topic.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
