e13fb2717f1d97be29b71eca991c21c43b5c38c5
Matt Wright <wrattmight@gmail.com>
2010-06-13 18:16:31 -0700

    added facebook login to forum

--- db/migrate/20100614011345_allow_nullon_users.rb
+++ db/migrate/20100614011345_allow_nullon_users.rb
@@ -1,9 +1,9 @@
 class AllowNullonUsers < ActiveRecord::Migration
   def self.up
-  	change_column :users, :password_salt, :string :null=>true 
+  	change_column :users, :password_salt, :string, :null=>true 
   end
 
   def self.down
-  	change_column :users, :password_salt, :string :null=>false
+  	change_column :users, :password_salt, :string, :null=>false
   end
 end
