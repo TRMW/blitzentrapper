5fab7a2e51e740ca280f2b7c9a0b6f6226256b3c
Matt Wright <wrattmight@gmail.com>
2010-06-13 18:38:10 -0700

    added facebook login to forum

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -4,8 +4,4 @@ class UserSession < Authlogic::Session::Base
   def self.oauth2_client
     OAuth2::Client.new("124824307554524", "588ac5177103796ec2af6380c7c26857", :site => "https://graph.facebook.com")
   end
-
-  def self.oauth2_scope
-  	''
-  end
 end
