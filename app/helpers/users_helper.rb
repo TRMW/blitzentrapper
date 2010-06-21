7b1fcccb418c9af54a0ede53042a4c24cc4f07c6
Matt @ IODA <wrattmight@gmail.com>
2010-06-21 13:45:58 -0700

    added helper function for avatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -18,4 +18,16 @@ module UsersHelper
   	tagline << 'since ' + content_tag(:strong, @user.created_at.strftime('%B %Y'))
   	tagline.join(' ')
   end
+  
+	def avatar(user, avatar_class)
+			if user.oauth2_token?
+				if avatar_class == :default
+					link_to image_tag('https://graph.facebook.com/' + user.fbid + '/picture?type=large', :class => 'avatar'), user_path(user)
+				else
+					link_to image_tag('https://graph.facebook.com/' + user.fbid + '/picture', :class => 'avatar'), user_path(user)
+				end
+			else
+				link_to image_tag(user.avatar.url(avatar_class), :class => 'avatar'), user_path(user)
+			end
+	end
 end
