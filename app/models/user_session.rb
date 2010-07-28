b3d624a042264034df31116c16faf2d611244505
Matt Wright <wrattmight@gmail.com>
2010-07-28 08:47:01 -0700

    user first formatting and attempted facebook fix

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,7 +1,7 @@
 class UserSession < Authlogic::Session::Base
 	verify_password_method :bbpress
 	oauth2_client_id      "124824307554524"
-  oauth2_client_secret  "APPLICATION_SECRET"
+  oauth2_client_secret  "588ac5177103796ec2af6380c7c26857"
   oauth2_site           "https://graph.facebook.com"
   oauth2_scope          "offline_access"
 end
