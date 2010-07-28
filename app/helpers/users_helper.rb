00dd0d29d3f47e2a6a12d48994ef5b2adbdc0f02
Matt Wright <wrattmight@gmail.com>
2010-07-28 09:01:32 -0700

    user first formatting and attempted facebook fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,4 @@
 module UsersHelper
-  def avatar_image_tag(slug, name)
-	  if File.exists?('public/images/avatars/' + slug + '.jpg')
-	  	image_tag 'avatars/' + slug + '.jpg', :class => "avatar", :title => name
-	  elsif File.exists?('public/images/avatars/' + slug + '.gif')
-	  	image_tag 'avatars/' + slug + '.gif', :class => "avatar", :title => name
-	  elsif File.exists?('public/images/avatars/' + slug + '.png')
-	  	image_tag 'avatars/' + slug + '.png', :class => "avatar", :title => name
-	  else image_tag 'avatars/btdefault.gif', :class => "avatar", :title => name
-	  end
-  end
   
   def user_tagline
   tagline = []
@@ -20,14 +10,15 @@ module UsersHelper
   end
   
 	def avatar(user, avatar_class)
-			if user.oauth2_token?
-				if avatar_class == :default
-					link_to image_tag('https://graph.facebook.com/' + user.fbid + '/picture?type=large', :class => 'avatar'), user_path(user)
-				else
-					link_to image_tag('https://graph.facebook.com/' + user.fbid + '/picture', :class => 'avatar'), user_path(user)
-				end
+		if user.oauth2_token?
+			if avatar_class == :default
+				link_to image_tag('https://graph.facebook.com/' + user.fbid + '/picture?type=large', :class => 'avatar'), user_path(user)
 			else
-				link_to image_tag(user.avatar.url(avatar_class), :class => 'avatar'), user_path(user)
+				link_to image_tag('https://graph.facebook.com/' + user.fbid + '/picture', :class => 'avatar'), user_path(user)
 			end
+		else
+			link_to image_tag(user.avatar.url(avatar_class), :class => 'avatar'), user_path(user)
+		end
 	end
+	
 end
