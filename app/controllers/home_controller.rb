954a303e476d213fd0a8d1e7eaecfc56610a61fb
Matt Wright <wrattmight@gmail.com>
2010-09-09 08:28:04 -0700

    adding fallback for when tumblr api is down

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,16 @@
 class HomeController < ApplicationController
 	def index
-		response = HTTParty.get('http://blitzentrapper.tumblr.com/api/read', :query => {:num => '10', :filter => 'none'})
-		@blogposts = response['tumblr']['posts']
 		@latest_release = Record.find(:last, :order => "release_date ASC")
 		@topics = Topic.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
 		@postedshows = Show.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
 		@shows = Show.today_forward.limit(3)
+		response = HTTParty.get('http://blitzentrapper.tumblr.com/api/read', :query => {:num => '10', :filter => 'none'})
+		if response['tumblr'].nil?
+			@records = Record.all(:order => 'release_date DESC')
+			render 'records/index' and return
+		else
+			@blogposts = response['tumblr']['posts']
+		end
 	end
 	
   #redirect to index
--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -6,6 +6,7 @@ class RecordsController < ApplicationController
   def show
   	if Record.find_by_slug(params[:id])
 	    @record = Record.find_by_slug(params[:id])
+	    response.headers['Cache-Control'] = 'public, max-age=600'
 	  else Record.find(params[:id]) # find (unlike dynamic finders) raises RecordNotFound automatically
 	  	@record = Record.find(params[:id])
 	  	redirect_to :action => 'show', :id => @record.slug, :status => :moved_permanently
