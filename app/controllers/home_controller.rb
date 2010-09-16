f319e6acc338d22104a494035afb8d5a37c145f7
Matt Wright <wrattmight@gmail.com>
2010-09-15 21:50:00 -0700

    set up caching for tumblr API call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,19 +4,21 @@ class HomeController < ApplicationController
 		@topics = Topic.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
 		@postedshows = Show.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
 		@shows = Show.today_forward.limit(3)
-		response = HTTParty.get('http://blitzentrapper.tumblr.com/api/read', :query => {:num => '10', :filter => 'none'})
-		if response['tumblr'].nil?
-			logger.error("ERROR: response['tumblr'] us nil while trying to access Tumblr API")
-			@records = Record.all(:order => 'release_date DESC')
-			render 'records/index'
-		else
+		
+		# get cached post unless cache is older than ten minutes old
+		cache_time = Rails.cache.read('tumblr_cache_saved_at')
+		if cache_time.nil? || cache_time < 10.minutes.ago
+			response = HTTParty.get('http://blitzentrapper.tumblr.com/api/read', :query => {:num => '10', :filter => 'none'})
 			@blogposts = response['tumblr']['posts']
+			Rails.cache.write('tumblr_cache', @blogposts)
+			Rails.cache.write('tumblr_cache_saved_at', Time.zone.now)
+		else
+			@blogposts = Rails.cache.read('tumblr_cache')
 		end
 		
 		rescue Net::HTTPBadResponse
 			logger.error("ERROR: Got Net::HTTPBadResponse when trying to access Tumblr API")
-			@records = Record.all(:order => 'release_date DESC')
-			render 'records/index' and return
+			@blogposts = Rails.cache.read('tumblr_cache')
 		else
 	end
 	
