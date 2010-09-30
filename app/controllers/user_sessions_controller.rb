d2021c6d1c288d10551617d38fa63586002efafc
Matt Wright <wrattmight@gmail.com>
2010-09-30 09:06:14 -0700

    store location before logout

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,7 +1,6 @@
 class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
-  before_filter :store_location, :only => :new
   
   def new
     @user_session = UserSession.new
