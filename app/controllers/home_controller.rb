bc5e383551110ee9cde7af5c7060cbfd0f287f5f
Matt Wright <wrattmight@gmail.com>
2010-11-01 19:17:05 -0700

    switching back to normal blog view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,27 +1,23 @@
 class HomeController < ApplicationController
-# 	def index
-# 		@latest_release = Record.find(:last, :order => "release_date ASC")
-# 		@topics = Topic.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
-# 		@postedshows = Show.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
-# 		@shows = Show.today_forward.limit(3)
-# 		response = HTTParty.get('http://blitzentrapper.tumblr.com/api/read', :query => {:num => '10', :filter => 'none'})
-# 		if response['tumblr'].nil?
-# 			logger.error("ERROR: response['tumblr'] us nil while trying to access Tumblr API")
-# 			@records = Record.all(:order => 'release_date DESC')
-# 			render 'records/index'
-# 		else
-# 			@blogposts = response['tumblr']['posts']
-# 		end
-# 		
-# 		rescue Net::HTTPBadResponse
-# 			logger.error("ERROR: Got Net::HTTPBadResponse when trying to access Tumblr API")
-# 			@records = Record.all(:order => 'release_date DESC')
-# 			render 'records/index' and return
-# 		else
-# 	end
-
 	def index
-		redirect_to '/tree'
+		@latest_release = Record.find(:last, :order => "release_date ASC")
+		@topics = Topic.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
+		@postedshows = Show.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
+		@shows = Show.today_forward.limit(3)
+		response = HTTParty.get('http://blitzentrapper.tumblr.com/api/read', :query => {:num => '10', :filter => 'none'})
+		if response['tumblr'].nil?
+			logger.error("ERROR: response['tumblr'] us nil while trying to access Tumblr API")
+			@records = Record.all(:order => 'release_date DESC')
+			render 'records/index'
+		else
+			@blogposts = response['tumblr']['posts']
+		end
+		
+		rescue Net::HTTPBadResponse
+			logger.error("ERROR: Got Net::HTTPBadResponse when trying to access Tumblr API")
+			@records = Record.all(:order => 'release_date DESC')
+			render 'records/index' and return
+		else
 	end
 		
   #redirect to index
