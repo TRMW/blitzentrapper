21fd7de4322fe1809003d30e5cdeac03f0a216a4
The Real Matt Wright <wrattmight@gmail.com>
2010-11-09 16:08:41 -0800

    switching to dalli memcache gem as per heroku's suggestion

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,11 +4,23 @@ class HomeController < ApplicationController
 		@topics = Topic.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
 		@postedshows = Show.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
 		@shows = Show.today_forward.limit(3)
-
-		response = HTTParty.get('http://blitzentrapper.tumblr.com/api/read', :query => {:num => '10', :filter => 'none'})
-		@blogposts = response['tumblr']['posts']
-		Rails.cache.write('tumblr_cache', @blogposts)
-		Rails.cache.write('tumblr_cache_saved_at', Time.zone.now)
+		
+		# serve cached posts unless cache is older than ten minutes
+		cache_time = Rails.cache.read('tumblr_cache_saved_at')
+		logger.info("tumblr_cache_saved_at = #{cache_time}")
+		if cache_time.to_date < 10.minutes.ago
+			response = HTTParty.get('http://blitzentrapper.tumblr.com/api/read', :query => {:num => '10', :filter => 'none'})
+			raise Net::HTTPBadResponse if response['tumblr'].nil?
+			@blogposts = response['tumblr']['posts']
+			Rails.cache.write('tumblr_cache', @blogposts)
+			Rails.cache.write('tumblr_cache_saved_at', Time.zone.now)
+		else
+			get_cached_posts_or_fallback
+		end
+		
+		# serve cached posts if Tumblr is failing
+		rescue Net::HTTPBadResponse
+			get_cached_posts_or_fallback
 	end
 	
 	def get_cached_posts_or_fallback
