4091ff9fad85a70f31951a3dfa7cacc0f85ebe7d
The Real Matt Wright <wrattmight@gmail.com>
2010-11-15 10:26:26 -0800

    add cancel_setlist route

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ ActionController::Routing::Routes.draw do |map|
   map.show_month 'shows/archive/:year/:month', :controller => 'shows', :action => 'month'
   map.connect 'shows/search', :controller => 'shows', :action => 'search'
   map.connect 'shows/edit_setlist', :controller => 'shows', :action => 'edit_setlist'
+  map.connect 'shows/cancel_setlist', :controller => 'shows', :action => 'cancel_setlist'
   
   # forum stuff
   map.connect 'forum.php', :controller => 'topics', :action => 'redirect_home'
