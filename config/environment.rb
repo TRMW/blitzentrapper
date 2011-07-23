4091f8d7a80e8097e9fbf8f8ca9f86014c800478
Matt Wright <wrattmight@gmail.com>
2011-07-23 15:22:52 -0700

    setting authlogic version

--- config/environment.rb
+++ config/environment.rb
@@ -21,7 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-	config.gem 'authlogic'
+	config.gem 'authlogic', :version => '2.1.6'
 	config.gem 'hpricot'
 	config.gem 'json'
 	config.gem 'httparty'
