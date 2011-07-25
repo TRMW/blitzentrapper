6501039ffff9ced1b41615898aa904a1c3404fae
Matt Wright <wrattmight@gmail.com>
2011-07-24 18:33:16 -0700

    upgrade to rails 3 - add new files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blitzen::Application.config.secret_token = '85ce87db30614402712833b809eed73b45e3a74b4bbc8a1ec610e5c6430630b141b7d3628d3147bb344d36f67f2aa2dcc3f759d524ac8fd2d9609b83e02870cd'
