cfc2785696935946e005ebaa0781706968f05090
The Real Matt Wright @ IODA <wrattmight@gmail.com>
2011-11-18 16:11:15 -0800

    title case Mentok so Menteer's posts are flagged as band member posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,12 +16,12 @@ class ApplicationController < ActionController::Base
     end
     
     def is_admin
-    	current_user && (['Matt', 'Sara', 'michael.james', 'marty', 'mentok', 'earley', 'E. Earley', 'Brian Trapper', 'Ross McLochness'].include? current_user.login)
+    	current_user && (['Matt', 'Sara', 'michael.james', 'marty', 'Mentok', 'earley', 'E. Earley', 'Brian Trapper', 'Ross McLochness'].include? current_user.login)
     end
     
     def is_band_member(user)
     	user = user || current_user
-    	['michael.james', 'marty', 'mentok', 'earley', 'E. Earley', 'Brian Trapper', 'Ross McLochness'].include? user.login
+    	['michael.james', 'marty', 'Mentok', 'earley', 'E. Earley', 'Brian Trapper', 'Ross McLochness'].include? user.login
     end
     
     def is_team_member(user)
