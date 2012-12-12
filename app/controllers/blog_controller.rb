29d4de15e63c4628609ced2d6fe09aad3d2d787a
Matt Wright <mattwright@squareup.com>
2012-12-12 15:31:55 -0800

    Add fallback if Tumblr is down and use spaces for indentation

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,55 +1,67 @@
 class BlogController < ApplicationController
-	def index
-		redirect_to :root
-	end
-	
-	def show
-		# serve cached post if it's available, otherwise hit Tumblr API
-		@post = Rails.cache.read('tumblr_cache').find { |post| post['id'] == params[:id] } || 
-						HTTParty.get('http://api.tumblr.com/v2/blog/blitzentrapper.tumblr.com/posts', 
-							:query => {
-								:api_key => 'Xx2F44h0x9f9lKcwSN9lVGbZ7y8MyRNl6HoDDOWa3zNR4PlyVP', 
-								:id => params[:id] })['response']['posts'][0]
-	end
-	
-	def page
-		@page = params[:page].to_i
-		start = @page * 10
-		tumblr = HTTParty.get('http://api.tumblr.com/v2/blog/blitzentrapper.tumblr.com/posts', 
-			:query => {
-				:api_key => 'Xx2F44h0x9f9lKcwSN9lVGbZ7y8MyRNl6HoDDOWa3zNR4PlyVP', 
-				:limit => '10', 
-				:offset => start })
-		@posts = tumblr['response']['posts']
-		@lastpage = true if tumblr['response']['blog']['posts'] < start + 11
-		render :index
-	end
-	
-	def videos
-		tumblr = HTTParty.get('http://api.tumblr.com/v2/blog/blitzentrapper.tumblr.com/posts', 
-			:query => {
-				:api_key => 'Xx2F44h0x9f9lKcwSN9lVGbZ7y8MyRNl6HoDDOWa3zNR4PlyVP',
-				:type => 'video',
-				:tag => 'video' })
-		@videos = []
-		
-		tumblr['response']['posts'].each do |post|
-			embed = post['player'][1]['embed_code']
-			
-			if embed.match('youtube')
-				video_id = embed.match(/embed\/(.*)\?/)[1]
-				youtube_info = HTTParty.get("https://gdata.youtube.com/feeds/api/videos/#{video_id}?v=2&alt=json")
-				post['title'] = youtube_info['entry']['title']['$t']
-				post['thumbnail'] = youtube_info['entry']['media$group']['media$thumbnail'][1]['url']
-				
-			elsif embed.match('vimeo')
-				video_id = embed.match(/video\/(\d*)/)[1]
-				vimeo_info = HTTParty.get("http://vimeo.com/api/v2/video/#{video_id}.json")
-				post['title'] = vimeo_info[0]['title']
-				post['thumbnail'] = vimeo_info[0]['thumbnail_large']
-			end
-			
-			@videos << post
-		end
-	end
+  def index
+    redirect_to :root
+  end
+
+  def show
+    # serve cached post if it's available, otherwise hit Tumblr API
+    @post = Rails.cache.read('tumblr_cache').find { |post| post['id'] == params[:id] } ||
+            HTTParty.get('http://api.tumblr.com/v2/blog/blitzentrapper.tumblr.com/posts',
+              :query => {
+                :api_key => 'Xx2F44h0x9f9lKcwSN9lVGbZ7y8MyRNl6HoDDOWa3zNR4PlyVP',
+                :id => params[:id] })['response']['posts'][0]
+
+    # redirect to records if Tumblr is down
+    rescue SocketError
+      render 'store/new'
+  end
+
+  def page
+    @page = params[:page].to_i
+    start = @page * 10
+    tumblr = HTTParty.get('http://api.tumblr.com/v2/blog/blitzentrapper.tumblr.com/posts',
+      :query => {
+        :api_key => 'Xx2F44h0x9f9lKcwSN9lVGbZ7y8MyRNl6HoDDOWa3zNR4PlyVP',
+        :limit => '10',
+        :offset => start })
+    @posts = tumblr['response']['posts']
+    @lastpage = true if tumblr['response']['blog']['posts'] < start + 11
+    render :index
+
+    # redirect to records if Tumblr is down
+    rescue SocketError
+      render 'store/new'
+  end
+
+  def videos
+    tumblr = HTTParty.get('http://api.tumblr.com/v2/blog/blitzentrapper.tumblr.com/posts',
+      :query => {
+        :api_key => 'Xx2F44h0x9f9lKcwSN9lVGbZ7y8MyRNl6HoDDOWa3zNR4PlyVP',
+        :type => 'video',
+        :tag => 'video' })
+    @videos = []
+
+    tumblr['response']['posts'].each do |post|
+      embed = post['player'][1]['embed_code']
+
+      if embed.match('youtube')
+        video_id = embed.match(/embed\/(.*)\?/)[1]
+        youtube_info = HTTParty.get("https://gdata.youtube.com/feeds/api/videos/#{video_id}?v=2&alt=json")
+        post['title'] = youtube_info['entry']['title']['$t']
+        post['thumbnail'] = youtube_info['entry']['media$group']['media$thumbnail'][1]['url']
+
+      elsif embed.match('vimeo')
+        video_id = embed.match(/video\/(\d*)/)[1]
+        vimeo_info = HTTParty.get("http://vimeo.com/api/v2/video/#{video_id}.json")
+        post['title'] = vimeo_info[0]['title']
+        post['thumbnail'] = vimeo_info[0]['thumbnail_large']
+      end
+
+      @videos << post
+    end
+
+    # redirect to records if Tumblr is down
+    rescue SocketError
+      render 'store/new'
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,77 +1,80 @@
 class HomeController < ApplicationController
-	def index
-		#redirect_to :action => 'massacre', :status => 307 #307 = Temporary Redirect
-		
-		@latest_release = Record.find(:last, :order => "release_date ASC")
-		@topics = Topic.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
-		@postedshows = Show.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
-		@shows = Show.today_forward.limit(3)
-		
-		# serve cached posts unless cache is older than ten minutes
-		cache_time = Rails.cache.read('tumblr_cache_saved_at')
-		logger.info("tumblr_cache_saved_at = #{cache_time}")
-		if cache_time.nil? || (cache_time.to_time < 10.minutes.ago)
-			tumblr = HTTParty.get('http://api.tumblr.com/v2/blog/blitzentrapper.tumblr.com/posts', 
-				:query => {
-					:api_key => 'Xx2F44h0x9f9lKcwSN9lVGbZ7y8MyRNl6HoDDOWa3zNR4PlyVP', 
-					:limit => '10'})
-			# raise Net::HTTPBadResponse if response['tumblr'].nil? || response['tumblr']['posts'].nil?
-			@blogposts = tumblr['response']['posts']
-			Rails.cache.write('tumblr_cache', @blogposts)
-			Rails.cache.write('tumblr_cache_saved_at', Time.zone.now)
-		else
-			get_cached_posts_or_fallback
-		end
+  def index
+    #redirect_to :action => 'massacre', :status => 307 #307 = Temporary Redirect
 
-		tumblr = HTTParty.get('http://api.tumblr.com/v2/blog/blitzentrapper.tumblr.com/posts', 
-			:query => {
-				:api_key => 'Xx2F44h0x9f9lKcwSN9lVGbZ7y8MyRNl6HoDDOWa3zNR4PlyVP', 
-				:limit => '10' })
-		@blogposts = tumblr['response']['posts']
-		Rails.cache.write('tumblr_cache', @blogposts)
-		Rails.cache.write('tumblr_cache_saved_at', Time.zone.now)
-		
-		# serve cached posts if Tumblr is failing
-		rescue Net::HTTPBadResponse
-			get_cached_posts_or_fallback
-	end
-	
-	def stream_auth
-		if request.post?
-			if /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.match(params[:email])
-				flash[:notice] = "Authorization successful!"
-				redirect_to '/vh3yT4zx?key=aZ6jklasX89aUc45'
-			else
-				flash[:error] = "Authorization failed!"
-			end
-		end
-	end
-	
-	def vh3yT4zx
-		if flash.blank?
-			flash[:error] = "Authorization failed!"
-			redirect_to :home
-		end
-	end
-	
-	def get_cached_posts_or_fallback
-		# check that cache isn't empty
-		if !Rails.cache.read('tumblr_cache').blank?
-			@blogposts = Rails.cache.read('tumblr_cache')
-			logger.info("Used cached Tumblr posts.")
-		# cache is empty so display records instead
-		else
-			logger.error("ERROR: Can't read cache, displaying records instead")
-			logger.info(Rails.cache.read('tumblr_cache'))
-			@records = Record.all(:order => 'release_date DESC')
-			render 'records/index'
-		end
-	end
-	
-	def presale
-		redirect_to '/american-goldwing-promo'
-	end
-	
+    @latest_release = Record.find(:last, :order => "release_date ASC")
+    @topics = Topic.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
+    @postedshows = Show.find(:all, :conditions => "last_post_date IS NOT NULL", :order => "last_post_date DESC", :limit => 3)
+    @shows = Show.today_forward.limit(3)
+
+    # serve cached posts unless cache is older than ten minutes
+    cache_time = Rails.cache.read('tumblr_cache_saved_at')
+    logger.info("tumblr_cache_saved_at = #{cache_time}")
+    if cache_time.nil? || (cache_time.to_time < 10.minutes.ago)
+      tumblr = HTTParty.get('http://api.tumblr.com/v2/blog/blitzentrapper.tumblr.com/posts',
+        :query => {
+          :api_key => 'Xx2F44h0x9f9lKcwSN9lVGbZ7y8MyRNl6HoDDOWa3zNR4PlyVP',
+          :limit => '10'})
+      # raise Net::HTTPBadResponse if response['tumblr'].nil? || response['tumblr']['posts'].nil?
+      @blogposts = tumblr['response']['posts']
+      Rails.cache.write('tumblr_cache', @blogposts)
+      Rails.cache.write('tumblr_cache_saved_at', Time.zone.now)
+    else
+      get_cached_posts_or_fallback
+    end
+
+    tumblr = HTTParty.get('http://api.tumblr.com/v2/blog/blitzentrapper.tumblr.com/posts',
+      :query => {
+        :api_key => 'Xx2F44h0x9f9lKcwSN9lVGbZ7y8MyRNl6HoDDOWa3zNR4PlyVP',
+        :limit => '10' })
+    @blogposts = tumblr['response']['posts']
+    Rails.cache.write('tumblr_cache', @blogposts)
+    Rails.cache.write('tumblr_cache_saved_at', Time.zone.now)
+
+    # serve cached posts if Tumblr is failing
+    rescue Net::HTTPBadResponse
+      get_cached_posts_or_fallback
+
+    rescue SocketError
+      get_cached_posts_or_fallback
+  end
+
+  def stream_auth
+    if request.post?
+      if /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.match(params[:email])
+        flash[:notice] = "Authorization successful!"
+        redirect_to '/vh3yT4zx?key=aZ6jklasX89aUc45'
+      else
+        flash[:error] = "Authorization failed!"
+      end
+    end
+  end
+
+  def vh3yT4zx
+    if flash.blank?
+      flash[:error] = "Authorization failed!"
+      redirect_to :home
+    end
+  end
+
+  def get_cached_posts_or_fallback
+    # check that cache isn't empty
+    if !Rails.cache.read('tumblr_cache').blank?
+      @blogposts = Rails.cache.read('tumblr_cache')
+      logger.info("Used cached Tumblr posts.")
+    # cache is empty so display records instead
+    else
+      logger.error("ERROR: Can't read cache, displaying records instead")
+      logger.info(Rails.cache.read('tumblr_cache'))
+      @records = Record.all(:order => 'release_date DESC')
+      render 'store/new'
+    end
+  end
+
+  def presale
+    redirect_to '/american-goldwing-promo'
+  end
+
   #redirect to index
   def redirect
     redirect_to :action => 'index', :status => :moved_permanently
