5019ec7b64ef6ccef729f8f22287a600608e5b87
Matt Wright <matt.c.wright@me.com>
2013-01-12 15:47:07 -0800

    Remove Products model (since we're grabbing from Topspin)

--- app/models/product.rb
+++
@@ -1,4 +0,0 @@
-class Product < ActiveRecord::Base
-  attr_accessible :name, :description, :buy_buttons, :image_file_name, :image
-  has_attached_file :image, :styles => { :medium => "240x240^" }, :default_style => :medium, :storage => :s3, :s3_credentials => "#{RAILS_ROOT}/config/s3.yml", :s3_host_alias => "files.blitzentrapper.net", :url => ":s3_alias_url", :path => "products/:name/:style.:extension"
-end
---
+++ db/migrate/20130112233748_remove_products.rb
@@ -0,0 +1,15 @@
+class RemoveProducts < ActiveRecord::Migration
+  def up
+    drop_table :products
+  end
+
+  def down
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.text :buy_buttons
+      t.string :image_file_name
+      t.timestamps
+    end
+  end
+end
