ae735bffe033ed8ab7121b2cad7477ce09d47936
Matt Wright <mattwright@squareup.com>
2015-08-30 16:39:21 -0700

    Order record track listing by track number

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,6 +1,6 @@
 class Record < ActiveRecord::Base
   has_many :tracklistings
-  has_many :songs, :through => :tracklistings
+  has_many :songs, :through => :tracklistings, :order => 'track_number ASC'
   accepts_nested_attributes_for :tracklistings,
     :allow_destroy => true,
     :reject_if => proc { |attributes| attributes['song_id'].blank? && attributes['song_attributes']['title'].blank? }
