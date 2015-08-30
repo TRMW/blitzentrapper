65ab029428ac87172eae4713451f10e4db1b93a3
Matt Wright <mattwright@squareup.com>
2015-08-30 14:46:55 -0700

    Update schema and Gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110912173035) do
+ActiveRecord::Schema.define(:version => 20130112233748) do
 
   create_table "posts", :force => true do |t|
     t.text     "body"
@@ -26,15 +27,6 @@ ActiveRecord::Schema.define(:version => 20110912173035) do
   add_index "posts", ["postable_type"], :name => "index_posts_on_postable_type"
   add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
   add_index "posts", ["visible"], :name => "index_posts_on_visible"
-
-  create_table "products", :force => true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.text     "buy_buttons"
-    t.string   "image_file_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "records", :force => true do |t|
     t.string   "title"
