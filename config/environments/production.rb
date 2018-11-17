d9d70eed5b6b4b1fadfdd59fb81c7e8033bc79e8
Matt Wright <mattwright@squareup.com>
2018-11-17 15:24:28 -0800

    Use disk for Active Storage in development

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
+  # Store uploaded files on Amazon in production
   config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
