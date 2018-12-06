70e1d756cb4d9e8f075f280489337b195cb15f0b
Matt Wright <mattwright@squareup.com>
2018-12-05 20:56:35 -0800

    Delete busted migration
    Fails saying column `datetime` doesn't exist, which appears to be true
    based on the history in the migrations

--- db/migrate/20100525024633_split_datetime.rb
+++
@@ -1,9 +0,0 @@
-class SplitDatetime < ActiveRecord::Migration[4.2]
-  def self.up
-  	rename_column :shows, :datetime, :date
-  end
-
-  def self.down
-  	rename_column :shows, :date, :datetime
-  end
-end
