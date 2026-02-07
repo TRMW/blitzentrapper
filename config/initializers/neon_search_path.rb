58b774a7d577dc82badb7b945eafbdf15a734b0d
Matt Wright <matt.c.wright@me.com>
2026-02-06 21:36:15 -0800

    Set search_path in configure_connection for Neon

--- config/initializers/neon_search_path.rb
+++ config/initializers/neon_search_path.rb
@@ -1,10 +1,11 @@
 # Neon (and some Postgres hosts) leave search_path unset. Neon's pooler rejects
-# search_path as a startup parameter, so set it after connect via a normal SQL command.
+# search_path as a startup parameter, so set it in configure_connection (runs
+# after the connection is established) via a normal SQL command.
 Rails.application.config.after_initialize do
   ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.prepend(Module.new do
-    def connect
-      super
+    def configure_connection
       execute("SET search_path TO public")
+      super
     end
   end)
 end
