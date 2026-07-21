44428227e397f099c4db8efb9ee4b4d1ecb23ed9
Railway Agent <railway@railway.com>
2026-07-21 01:28:42 +0000

    Address all Hound linter feedback
    - Remove trailing blank lines and whitespace
    - Extract get_shows into focused private class methods
    - Use == instead of === for case equality
    - Use single quotes for non-interpolated strings
    - Use private_class_method for class methods
    - Break long lines under 80 chars
    - Reduce method complexity and line counts

--- app/models/show.rb
+++ app/models/show.rb
@@ -52,57 +52,69 @@ class Show < ActiveRecord::Base
 
   def self.get_shows(date = nil)
     saved_shows = []
-    # grab shows from Bandsintown API
-    if date
-      bit_shows = JSON.parse(
-        URI.open(
-          "https://rest.bandsintown.com/artists/Blitzen%20Trapper/events?" \
-          "app_id=blitzentrapper&date=#{date}"
-        ).read
-      )
-    else
-      bit_shows = JSON.parse(
-        URI.open(
-          'https://rest.bandsintown.com/artists/Blitzen%20Trapper/events?' \
-          'app_id=blitzentrapper'
-        ).read
-      )
-    end
-
-    bit_shows.each do |received_show|
-      show = find_or_initialize_by(bit_id: received_show['id'])
-
-      unless show.manual?
-        datetime = received_show['datetime'].split('T') #split datetime into date and time
-        show.date = datetime.first # set or update time
-        show.time = datetime.last # set or update time
-        show.venue = received_show['venue']['name']
-        ticket_offer = received_show['offers'].find do |offer|
-          offer['type'] === 'Tickets'
-        end
-        if ticket_offer
-          show.ticket_link = ticket_offer['url'] + '?affil_code=blitzentrapper'
-          show.status = ticket_offer['status']
-        end
-        show.city = received_show['venue']['city']
-        show.country = received_show['venue']['country']
-        show.region = received_show['venue']['region']
-        show.latitude = received_show['venue']['latitude']
-        show.longitude = received_show['venue']['longitude']
-        show.bit_id = received_show['id']
-        saved_shows << show.id if (show.new_record? && !saved_shows.include?(show.id))
-        show.save!
-      end # end manual check
-
-    end # end Bandsintown loop
-    message = "Grabbed #{bit_shows.length} shows from Bandsintown and " \
-              "created #{saved_shows.length} new shows."
-    logger.info message
-    message
+    bit_shows = fetch_bandsintown_shows(date)
+    process_bandsintown_shows(bit_shows, saved_shows)
+    log_import_summary(bit_shows, saved_shows)
   end
 
   def self.get_archive_starting_year
     by_year(Date.today.beginning_of_year).first ? Date.today.year : Date.today.year - 1
+  end
+
+  private_class_method
+
+  def self.fetch_bandsintown_shows(date)
+    if date
+      url = 'https://rest.bandsintown.com/artists/Blitzen%20Trapper/events?' \
+            "app_id=blitzentrapper&date=#{date}"
+    else
+      url = 'https://rest.bandsintown.com/artists/Blitzen%20Trapper/events?' \
+            'app_id=blitzentrapper'
+    end
+
+    JSON.parse(URI.open(url).read)
+  end
+
+  def self.process_bandsintown_shows(bit_shows, saved_shows)
+    bit_shows.each do |received_show|
+      show = find_or_initialize_by(bit_id: received_show['id'])
+
+      next if show.manual?
+
+      update_show_from_bandsintown(show, received_show)
+      saved_shows << show.id if show.new_record? && !saved_shows.include?(show.id)
+      show.save!
+    end
+  end
+
+  def self.update_show_from_bandsintown(show, received_show)
+    datetime = received_show['datetime'].split('T')
+    show.date = datetime.first
+    show.time = datetime.last
+    show.venue = received_show['venue']['name']
+
+    ticket_offer = received_show['offers'].find do |offer|
+      offer['type'] == 'Tickets'
+    end
+
+    if ticket_offer
+      show.ticket_link = ticket_offer['url'] + '?affil_code=blitzentrapper'
+      show.status = ticket_offer['status']
+    end
+
+    show.city = received_show['venue']['city']
+    show.country = received_show['venue']['country']
+    show.region = received_show['venue']['region']
+    show.latitude = received_show['venue']['latitude']
+    show.longitude = received_show['venue']['longitude']
+    show.bit_id = received_show['id']
+  end
+
+  def self.log_import_summary(bit_shows, saved_shows)
+    message = "Grabbed #{bit_shows.length} shows from Bandsintown and " \
+              "created #{saved_shows.length} new shows."
+    logger.info message
+    message
   end
 end
 
